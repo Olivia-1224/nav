c54fb93f799628bf64912303c9ddd200443aae06
874489183@qq.com <874489183@qq.com>
2023-03-23 20:09:01 +0800

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,23 @@
+# CMake generated Testfile for 
+# Source directory: /home/olivia/nav_ws/src
+# Build directory: /home/olivia/nav_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("navigation/src/navigation")
+subdirs("navigation/src/map_server")
+subdirs("navigation/src/amcl")
+subdirs("navigation/src/fake_localization")
+subdirs("navigation/src/voxel_grid")
+subdirs("navigation/src/costmap_2d")
+subdirs("navigation/src/nav_core")
+subdirs("navigation/src/base_local_planner")
+subdirs("navigation/src/carrot_planner")
+subdirs("navigation/src/clear_costmap_recovery")
+subdirs("navigation/src/dwa_local_planner")
+subdirs("navigation/src/move_slow_and_clear")
+subdirs("navigation/src/navfn")
+subdirs("navigation/src/global_planner")
+subdirs("navigation/src/rotate_recovery")
+subdirs("navigation/src/move_base")
---
+++ build/navigation/src/global_planner/CMakeFiles/sdf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdf.dir/src/sdf.cpp.o"
+  "/home/olivia/nav_ws/devel/lib/global_planner/sdf.pdb"
+  "/home/olivia/nav_ws/devel/lib/global_planner/sdf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
